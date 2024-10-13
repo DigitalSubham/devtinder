62389d6fd09421bcdff6432afa181115ee54a268
Subham Kumar <shubhamkr354@gmail.com>
2024-10-13 15:14:09 +0530

    create a express server

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+app.use("/test", (req, res) => {
+  res.send("Hello from Server");
+});
+app.listen(3000, () => {
+  console.log("Server is started at 3000");
+});
